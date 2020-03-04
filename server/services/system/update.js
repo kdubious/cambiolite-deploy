58a859d686e8776f3b28b9c2cbcf77ff5a4c26d6
Kevin Welsh <kwelsh@welshtechnologies.com>
2020-03-04 14:33:09 -0500

    test 2

--- server/services/system/update.js
+++ server/services/system/update.js
@@ -28,7 +28,7 @@ const doUpdate = () => __awaiter(void 0, void 0, void 0, function* () {
     const rslt = yield shell_1.default.executeAsync("GIT_SSH_COMMAND='ssh -i /opt/ssh.key' git pull");
     const updated = rslt.indexOf("Already up to date.") === -1;
     if (updated) {
-        logging_1.default.log("** restarting node", logging_1.default.LoggingCategories.SERVICES);
+        logging_1.default.log("* restarting node", logging_1.default.LoggingCategories.SERVICES);
         shell_1.default.spawn("/etc/init.d/S42node", ["start"], {
             detached: true,
             stdio: ["ignore"],
